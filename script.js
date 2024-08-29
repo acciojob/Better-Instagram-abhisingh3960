d03975ea3aa152766d0734bd3c99eb8e879cb259
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-29 15:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your code here
+const images = document.querySelectorAll('.image');
+
+images.forEach(image => {
+  image.addEventListener('dragstart', dragStart); 
+  image.addEventListener('dragover', dragOver);   
+  image.addEventListener('drop', drop);          
+});
+
+function dragStart(event) {
+  event.dataTransfer.setData('text/plain', event.target.innerText); 
+}
+
+
+function dragOver(event) {
+  event.preventDefault(); 
+}
+function drop(event) {
+  event.preventDefault(); 
+  const draggedText = event.dataTransfer.getData('text'); 
+
+	const targetText = event.target.innerText;
+     event.target.innerText = draggedText;
+
+	const draggedElement = [...images].find(img => img.innerText === draggedText);
+    draggedElement.innerText = targetText;
+}
